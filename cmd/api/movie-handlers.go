81c32b7c23120e1455f33e21ac3cd89d30654c12
Aviroute <youturncorporation@gmail.com>
2021-12-19 15:36:28 -0500

    adding

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -122,6 +122,13 @@ func (app *application) editmovie(w http.ResponseWriter, r *http.Request) {
 
 	var movie models.Movie
 
+	if payload.ID != "0" {
+		id, _ := strconv.Atoi(payload.ID)
+		m, _ := app.models.DB.Get(id)
+		movie = *m
+		movie.UpdatedAt = time.Now()
+	}
+
 	movie.ID, _ = strconv.Atoi(payload.ID)
 	movie.Title = payload.Title
 	movie.Description = payload.Description
@@ -133,16 +140,18 @@ func (app *application) editmovie(w http.ResponseWriter, r *http.Request) {
 	movie.CreatedAt = time.Now()
 	movie.UpdatedAt = time.Now()
 
-	log.Println(movie.Year)
-
-	err = app.models.DB.InsertMovie(movie)
-	if err != nil {
-		app.errorJSON(w, err)
-		return
-	}
-
-	type jsonResp struct {
-		OK bool `json:"ok"`
+	if movie.ID == 0 {
+		err = app.models.DB.InsertMovie(movie)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+	} else {
+		err = app.models.DB.UpdateMovie(movie)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
 	}
 
 	ok := jsonResp{
