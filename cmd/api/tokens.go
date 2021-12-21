9baf21bc3f12909672a88111234638f06cb1aad8
Aviroute <youturncorporation@gmail.com>
2021-12-20 23:48:01 -0500

    adding

---
+++ cmd/api/tokens.go
@@ -0,0 +1,9 @@
+package main
+
+import "backend/models"
+
+var validUser = models.User{
+	ID:       10,
+	Email:    "me@here.com",
+	Password: "$2a$12$3LHKNK/t.Zu409uUl3ItGevIXsoCZaMxoagFpJHfubhv/jvHN8AEK",
+}
