96fcc85f7bc099897ffd08cdc86d0bfbbe29687a
Aviroute <youturncorporation@gmail.com>
2021-12-20 23:59:29 -0500

    adding

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -1,9 +1,49 @@
 package main
 
-import "backend/models"
+import (
+	"backend/models"
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 var validUser = models.User{
 	ID:       10,
 	Email:    "me@here.com",
 	Password: "$2a$12$3LHKNK/t.Zu409uUl3ItGevIXsoCZaMxoagFpJHfubhv/jvHN8AEK",
+}
+
+type Credentials struct {
+	Username string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (app *application) Signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		app.errorJSON(w, errors.New("unauthorized"))
+		return
+	}
+
+	hashedPassword := validUser.Password
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), [byte(creds.Password))
+	if err != nil {
+		app.errorJSON(w, errors.new("unauthorized"))
+		return
+	}
+	var claims jwt.Claims
+	claims.Subject = fmt.Sprint(validUser.ID)
+	claims.Issued = jwt.NewNumericTime(time.Now())
+	claims.NotBeofre = jwt.NewNumericTime(time.Now())
+	claims.Expires = jwt.NewNumericTime(time.Now().Add(24 * time.Hour))
+	claims.Issuer = "mydomain.com"
+	claims.Audiences = []string{"mydomain.com"}
+
+	jwtBytes, err := claims.HMACSign(jwt.HS256, []byte(app.config.jwt.secret))
+
 }
